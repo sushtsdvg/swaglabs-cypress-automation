2c3a5e689638c48d9268072877ec5ef7dc2a3749
sushtsdvg <sushtsdvg@gmail.com>
2026-06-17 17:13:02 +0530

    feat(): inventory and cart pages

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
+    testIsolation: false,
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
