3e856c5303f6a040cb6803cc0be22a4fa3777c4f
sushtsdvg <sushtsdvg@gmail.com>
2026-06-18 12:43:15 +0530

    feat(): checkout

--- cypress/e2e/a_login.cy.js
+++ cypress/e2e/a_login.cy.js
@@ -1,26 +1,27 @@
+const loginData = require('../fixtures/login.json')
 describe('Login Tests', () =>{
     it('Invalid Username',()=>{
         cy.visit('/')
         cy.get('#login-button').click()
         cy.get('[data-test="error"]').should('be.visible').and('contain','Username is required')
-        cy.get('#user-name').type('standard_user')
+        cy.get('#user-name').type(loginData.username)
         cy.get('#login-button').click()
         cy.get('[data-test="error"]').should('be.visible').and('contain','Password is required')
         cy.reload()
 
         // Username and password error
-        cy.login('invalid_user', 'secret_sauce')
+        cy.login(loginData.invalid_username, loginData.password)
         cy.get('[data-test="error"]').should('be.visible').and('contain','Username and password do not match any user in this service')
         cy.reload()
 
         // Lockedout user error
-        cy.login('locked_out_user', 'secret_sauce')
+        cy.login(loginData.locked_out_user, loginData.password)
         cy.get('[data-test="error"]').should('be.visible').and('contain','Sorry, this user has been locked out.')
         cy.reload()
     })
     it('Verify Login with valid credentials',()=>{
         cy.visit('/')
-        cy.login('standard_user', 'secret_sauce')
+        cy.login(loginData.username, loginData.password)
         cy.url().should('include','/inventory.html')
     })
 })
